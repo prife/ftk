574c31108ac03a1cd413e2f2b946081efc0180ef
xianjimli <xianjimli@gmail.com>
2010-03-09 14:51:49 +0000

    add lua_ftk_callbacks.h

---
+++ script_binding/lua/lua_ftk_callbacks.h
@@ -0,0 +1,16 @@
+
+#ifndef LUA_FTK_CALLBACKS_H
+#define LUA_FTK_CALLBACKS_H
+
+Ret lua_ftk_listener_func(void* user_data, void* obj);
+int lua_ftk_compare_func(const void* obj1, const void* obj2);
+Ret lua_ftk_on_event_func(void* user_data, FtkEvent* event);
+Ret  lau_ftk_widget_on_event_func(FtkWidget* thiz, FtkEvent* event);
+Ret lua_ftk_prepare_options_menu_func(void* ctx, FtkWidget* menu_panel);
+void lua_ftk_destroy_func(void* user_data);
+Ret lua_ftk_idle_func(void* user_data);
+Ret lua_ftk_timer_func(void* user_data);
+
+void lua_callbacks_init(lua_State* L);
+
+#endif/*LUA_FTK_CALLBACKS_H*/
