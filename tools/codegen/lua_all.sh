1f4cbb35727772f209d2a9fc18e949783b1185d4
xianjimli <xianjimli@gmail.com>
2010-03-09 00:01:34 +0000

    refactor luagen

--- tools/codegen/lua_all.sh
+++ tools/codegen/lua_all.sh
@@ -5,3 +5,9 @@ do
 	echo $f
 	./luagen $f
 done
+
+./luagen idl/ftk_typedef.idl 1
+
+cp -fv lua_ftk*.h  ../../script_binding/lua/
+cp -fv lua_ftk*.c  ../../script_binding/lua/
+
---
+++ tools/codegen/lua_ftk_callbacks.h
@@ -0,0 +1,13 @@
+
+#ifndef LUA_FTK_CALLBACKS_H
+#define LUA_FTK_CALLBACKS_H
+
+Ret lua_ftk_listener_func(void* user_data, void* obj);
+int lua_ftk_compare_func(const void* obj1, const void* obj2);
+Ret lua_ftk_on_event_func(void* user_data, FtkEvent* event);
+Ret  lau_ftk_widget_on_event_func(FtkWidget* thiz, FtkEvent* event);
+void lua_ftk_destroy_func(void* user_data);
+Ret lua_ftk_idle_func(void* user_data);
+Ret lua_ftk_timer_func(void* user_data);
+
+#endif/*LUA_FTK_CALLBACKS_H*/
