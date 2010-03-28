2b5c060c8e784c61a30c6ee82ccbf2b1c1df8f1f
xianjimli <xianjimli@gmail.com>
2010-03-28 08:38:34 +0000

    modify some Makefile.am, prepare to release 0.4

--- config.h
+++ config.h
@@ -78,7 +78,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.3"
+#define VERSION "0.4"
 
 /* Define to `__inline__' or `__inline' if that's what the C compiler
    calls it, or to nothing if 'inline' is not supported under any name.  */
