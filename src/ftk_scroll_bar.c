44ac0d13b9730e0bd4b115554a3165c924d73a85
xianjimli <xianjimli@gmail.com>
2010-09-03 13:29:01 +0000

    fix scrollbar paint bug

--- src/ftk_scroll_bar.c
+++ src/ftk_scroll_bar.c
@@ -190,9 +190,9 @@ static Ret ftk_scroll_bar_on_paint(FtkWidget* thiz)
 		ftk_canvas_draw_bitmap(canvas, priv->bitmap, 0, 0, bitmap_width, half_size, dx, dy);
 		for(i = 0; i < fill_size; i++)
 		{
-			ftk_canvas_draw_bitmap(canvas, priv->bitmap, 0, half_size, bitmap_width, 1, dx, dy + i);
+			ftk_canvas_draw_bitmap(canvas, priv->bitmap, 0, half_size, bitmap_width, 1, dx, dy + i + half_size);
 		}
-		ftk_canvas_draw_bitmap(canvas, priv->bitmap, 0, half_size, bitmap_width, half_size, dx, dy + fill_size);
+		ftk_canvas_draw_bitmap(canvas, priv->bitmap, 0, half_size, bitmap_width, half_size, dx, dy + half_size + fill_size);
 	}
 	else
 	{
@@ -212,9 +212,9 @@ static Ret ftk_scroll_bar_on_paint(FtkWidget* thiz)
 		ftk_canvas_draw_bitmap(canvas, priv->bitmap, 0, 0, half_size, bitmap_height, dx, dy);
 		for(i = 0; i < fill_size; i++)
 		{
-			ftk_canvas_draw_bitmap(canvas, priv->bitmap, half_size, 0, 1, bitmap_height, dx + i, dy);
+			ftk_canvas_draw_bitmap(canvas, priv->bitmap, half_size, 0, 1, bitmap_height, dx + i + half_size, dy);
 		}
-		ftk_canvas_draw_bitmap(canvas, priv->bitmap, half_size, 0, half_size, bitmap_height, dx + fill_size, dy);
+		ftk_canvas_draw_bitmap(canvas, priv->bitmap, half_size, 0, half_size, bitmap_height, dx + fill_size + half_size, dy);
 	}
 
 	FTK_END_PAINT();
