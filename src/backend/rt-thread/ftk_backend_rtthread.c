b6e4aeaa65c18358bd8f16c787110033481ca4a8
qiuyi <qiuyiuestc@gmail.com>
2011-05-24 00:46:25 +0000

    add keypad support for RT-Thread porting

--- src/backend/rt-thread/ftk_backend_rtthread.c
+++ src/backend/rt-thread/ftk_backend_rtthread.c
@@ -3,6 +3,7 @@
 #include "ftk_globals.h"
 #include "ftk_display_rtthread.h"
 #include "ftk_source_touch.h"
+#include "ftk_source_input.h"
 
 static void ftk_source_touch_init(void)
 {
@@ -21,13 +22,13 @@ static void ftk_source_input_init(void)
 {
 	FtkSource* source = NULL;
 
-	source = ftk_source_input_create("keypad",
+	source = ftk_source_input_create(FTK_INPUT_NAME,
 		(FtkOnEvent)ftk_wnd_manager_queue_event_auto_rotate, ftk_default_wnd_manager());
 	if (source != NULL)
 	{
 		ftk_sources_manager_add(ftk_default_sources_manager(), source);
 	}
-	ftk_logd("%s: touch dev %s source=%p\n", __func__, FTK_TOUCH_NAME, source);
+	ftk_logd("%s: touch dev %s source=%p\n", __func__, FTK_INPUT_NAME, source);
 }
 
 Ret ftk_backend_init(int argc, char* argv[])
